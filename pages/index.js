485844e569aa098465dff88b49043943bd138edc
Euan Munro <euanmunroo@gmail.com>
2026-06-03 16:36:17 +0100

    making changes to analytics

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Cart from '../components/Cart';
 import MenuItem from '../components/MenuItem';
 import OrderForm from '../components/OrderForm';
 import CateringModal from '../components/CateringModal';
+import { Analytics } from "@vercel/analytics/next"
 
 const CATEGORIES = [
   { name: 'Starters',       icon: '' },
